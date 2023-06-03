ac447d7558d207256596dd47141cfe3d8f61d349
WriterV2 <primary-v2@protonmail.com>
2023-06-03 15:10:27 +0200

    Add constants for story file names

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use self::works::Works;
 
 mod works;
 
+const STORY: (&str, &str) = ("stories", "story");
+
 fn main() -> Result<()> {
     // new tera instance
     let tera = Tera::new("templates/**/*.html").context("Failed to create new Tera instance")?;
@@ -19,10 +21,10 @@ fn main() -> Result<()> {
     write("docs/index.html", output).context("Failed to write index.html")?;
 
     // render pages for stories
-    Stories::new_from_file("stories")?
-        .render_overview_page(&tera, "stories")
+    Stories::new_from_file(STORY.0)?
+        .render_overview_page(&tera, STORY.0)
         .context("Failed to render overview page for stories")?
-        .render_single_pages(&tera, "story")
+        .render_single_pages(&tera, STORY.1)
         .context("Failed to render single pages for stories")?;
 
     Ok(())
