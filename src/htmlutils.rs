6a32c534adf358b76dbe4076b58e7949f86cbce7
WriterV2 <primary-v2@protonmail.com>
2023-09-01 12:13:30 +0200

    Move base templating to seperate functions + return builders instead of components

--- src/htmlutils.rs
+++ src/htmlutils.rs
@@ -1,7 +1,8 @@
 use chrono::Datelike;
-use html::content::builders::MainBuilder;
+use html::content::builders::{FooterBuilder, MainBuilder, NavigationBuilder};
 use html::content::{Footer, Header, Heading1, Navigation};
 use html::inline_text::{Anchor, Underline};
+use html::metadata::builders::HeadBuilder;
 use html::metadata::{Head, Link, Meta, Title};
 use html::root::builders::HtmlBuilder;
 use html::root::Html;
@@ -12,14 +13,71 @@ const PAGE_TITLE_BASE: &str = "Vithuran Vishnuthas";
 // TODO: get routes dynamically
 const ROUTES: Vec<Vec<&str>> = vec![vec!["Home", "/"]];
 
+// Get HTML builder and add HTML components to it
+fn build_html() -> HtmlBuilder {
+    let mut html = Html::builder();
+    html
+}
+
+// Build an HTML head section with the provided meta information
+// (build and push to an HTML builder)
+fn build_head(keywords: &Vec<&str>, title: &str) -> HeadBuilder {
+    let page_title = if title.is_empty() {
+        PAGE_TITLE_BASE
+    } else {
+        &format!("{} - {}", PAGE_TITLE_BASE, title)
+    };
+
+    let keywords_meta = Meta::builder().name("keywords");
+    let mut content = String::new();
+    for keyword in keywords.iter() {
+        content += &format!("{},", keyword);
+    }
+    content.trim_end_matches(",");
+    keywords_meta.content(content);
+
+    *Head::builder()
+        .push(Title::builder().title(page_title).build())
+        .push(Meta::builder().charset("UTF-8").build())
+        .push(
+            Meta::builder()
+                .name("viewport")
+                .content("width=device-width, initial-scale=1")
+                .build(),
+        )
+        .push(keywords_meta.build())
+        .push(
+            Meta::builder()
+                .name("author")
+                .content("Vithuran Vishnuthas")
+                .build(),
+        )
+        .push(Link::builder().rel("stylesheet").href(STYLE_PATH).build())
+        .push(Link::builder().rel("icon").href(ICON_PATH).build())
+}
+
+// Build an HTML navigation with the provided links
+// (build and push to an HTML builder)
+fn build_navigation() -> NavigationBuilder {
+    let mut navigation = Navigation::builder();
+    for pair in ROUTES.iter() {
+        navigation.push(Anchor::builder().href(pair[1]).push(pair[0]).build());
+    }
+    navigation
+}
+
+// Build an HTML footer with an auto-updating copyright signature
+// (build and push to an HTML builder)
+fn build_footer() -> FooterBuilder {
+    *Footer::builder().push(format!(
+        "Vithuran Vishnuthas &copy; 2022 - {}",
+        chrono::Utc::now().year()
+    ))
+}
+
 trait GenerateHTMLGroup<T: GenerateHTMLPage> {
     fn get_pages() -> Vec<T>;
     fn build_overview_page() -> HtmlBuilder;
-    fn build_single_pages(pages: &Vec<T>) {
-        for page in pages.iter() {
-            page.build_single_page_content();
-        }
-    }
 }
 
 trait GenerateHTMLPage {
@@ -28,26 +86,6 @@ trait GenerateHTMLPage {
     fn get_tags(&self) -> Vec<String>;
     fn get_title(&self) -> String;
     fn get_description(&self) -> String;
-
-    fn build_html() -> HtmlBuilder {
-        let mut html = Html::builder();
-        html
-    }
-
-    fn build_head() -> Head {
-        Head::builder()
-            .push(Title::builder().title(PAGE_TITLE_BASE).build())
-            .push(Meta::builder().charset("UTF-8").build())
-            .push(
-                Meta::builder()
-                    .name("viewport")
-                    .content("width=device-width, initial-scale=1")
-                    .build(),
-            )
-            .push(Link::builder().rel("stylesheet").href(STYLE_PATH).build())
-            .push(Link::builder().rel("icon").href(ICON_PATH).build())
-            .build()
-    }
 
     fn build_header() -> Header {
         Header::builder()
@@ -62,23 +100,6 @@ trait GenerateHTMLPage {
                     .build(),
             )
             .push("Vithuran Vishnuthas")
-            .build()
-    }
-
-    fn build_navigation() -> Navigation {
-        let mut navigation = Navigation::builder();
-        for pair in ROUTES.iter() {
-            navigation.push(Anchor::builder().href(pair[1]).push(pair[0]).build());
-        }
-        navigation.build()
-    }
-
-    fn build_footer() -> Footer {
-        Footer::builder()
-            .push(format!(
-                "Vithuran Vishnuthas &copy; 2022 - {}",
-                chrono::Utc::now().year()
-            ))
             .build()
     }
 }
