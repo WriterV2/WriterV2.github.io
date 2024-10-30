61f784a5fb437489d5dd3f629618d49279ff15a5
WriterV2 <primary-v2@protonmail.com>
2024-10-30 17:04:26 +0100

    Feat: Add db handler method to get all records from table + implement for story

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,3 +1,8 @@
+use sqlx::SqlitePool;
+
+use crate::error::AppError;
+use async_trait::async_trait;
+
 pub mod game;
 pub mod gametag;
 pub mod product;
@@ -6,3 +11,10 @@ pub mod storytag;
 pub mod tag;
 pub mod tool;
 pub mod tooltag;
+
+#[async_trait]
+pub trait DatabaseHandler {
+    async fn get_all(pool: SqlitePool) -> Result<Vec<Self>, AppError>
+    where
+        Self: Sized;
+}
--- src/db/story.rs
+++ src/db/story.rs
@@ -1,3 +1,10 @@
+use axum::async_trait;
+use sqlx::SqlitePool;
+
+use crate::error::AppError;
+
+use super::DatabaseHandler;
+
 #[derive(Debug, serde::Serialize, sqlx::FromRow)]
 pub struct Story {
     pub id: i64,
@@ -5,4 +12,16 @@ pub struct Story {
     pub pdf: Vec<u8>,
     pub epub: Vec<u8>,
     pub pid: i64,
+}
+
+#[async_trait]
+impl DatabaseHandler for Story {
+    async fn get_all(pool: SqlitePool) -> Result<Vec<Self>, AppError>
+    where
+        Self: Sized,
+    {
+        Ok(sqlx::query_as!(Story, "SELECT * FROM story")
+            .fetch_all(&pool)
+            .await?)
+    }
 }
